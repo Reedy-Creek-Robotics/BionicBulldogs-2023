3abac4dc9a318291bb330a08bb2e7d0c5f512f83
eliro <eliroorda@gmail.com>
2023-09-26 15:55:30 -0400

    fixed FR by renaming vars changed Imu to ImuEx

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/hardware/ImuEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/hardware/ImuEx.java
@@ -1,12 +1,9 @@
 package org.firstinspires.ftc.teamcode.modules.hardware;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 public class ImuEx {
     IMU imu;
