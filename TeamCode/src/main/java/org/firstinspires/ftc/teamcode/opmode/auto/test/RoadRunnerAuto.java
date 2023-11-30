a30c614ac16867ab707f707e5e095d5155771b8b
eliro <eliroorda@gmail.com>
2023-11-30 18:47:23 -0500

    Fixed Slides and claw code and tested one meepmeep auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/test/RoadRunnerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/test/RoadRunnerAuto.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmode.auto.test;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
@@ -8,15 +9,29 @@ import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySe
 @Autonomous
 public class RoadRunnerAuto extends AutoBase {
     public TrajectorySequence getTrajectory(SampleMecanumDrive drive){
-        Pose2d startPos = new Pose2d(12, 63, Math.toRadians(-90));
+        Pose2d startPos = new Pose2d(-36, 66, Math.toRadians(-90));
+        drive.setPoseEstimate(startPos);
 
-        TrajectorySequence sequence = drive.trajectorySequenceBuilder(startPos)
-                .forward(30)
-                .lineToLinearHeading(new Pose2d(44, 35.2, Math.toRadians(180)))
-                .forward(100)
-                .back(100)
-                .forward(100)
-                .back(100)
+        TrajectorySequence sequence = drive.trajectorySequenceBuilder(new Pose2d(-36, 66, Math.toRadians(270)))
+                .lineToLinearHeading(new Pose2d(-30, 36, Math.toRadians(180)))
+                .lineToLinearHeading(new Pose2d(-60, 36, Math.toRadians(360)))
+                .splineTo(new Vector2d(-24, 60), Math.toRadians(360))
+                .splineTo(new Vector2d(12, 60), Math.toRadians(360))
+                .splineTo(new Vector2d(48, 36), Math.toRadians(360))
+                .turn(Math.toRadians(90))
+                .splineTo(new Vector2d(12, 60), Math.toRadians(180))
+                .splineTo(new Vector2d(-24, 60), Math.toRadians(180))
+                .lineToLinearHeading(new Pose2d(-60, 36, Math.toRadians(360)))
+                .splineTo(new Vector2d(-24, 60), Math.toRadians(360))
+                .splineTo(new Vector2d(12, 60), Math.toRadians(360))
+                .splineTo(new Vector2d(48, 36), Math.toRadians(360))
+                .turn(Math.toRadians(90))
+                .splineTo(new Vector2d(12, 60), Math.toRadians(180))
+                .splineTo(new Vector2d(-24, 60), Math.toRadians(180))
+                .lineToLinearHeading(new Pose2d(-60, 36, Math.toRadians(360)))
+                .splineTo(new Vector2d(-24, 60), Math.toRadians(360))
+                .splineTo(new Vector2d(12, 60), Math.toRadians(360))
+                .splineTo(new Vector2d(48, 36), Math.toRadians(360))
                 .build();
         return sequence;
     }
