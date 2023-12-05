0fb0ec32f94bd023dbb72880294e4557832177db
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-12-05 17:17:11 -0500

    Fixed claw and slides in main telop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/Slides.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.modules.robot;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.modules.hardware.MotorGroup;
@@ -8,15 +9,19 @@ import org.firstinspires.ftc.teamcode.opmode.config.SlideConfig;
 
 public class Slides {
     DcMotor motor;
+    Servo servo;
     int lastPosition = 0;
     int pixelHeight = 20; //encoder ticks to go up one pixel
-    float slideSpeed = 0.5f;
-
+    int slidePosToMoveServo = -1000;
+    float slideSpeed = 0.8f;
+    float servoStartPos = 0.47f;
+    float servoScorePos = 0.2f;
     public Slides(SlideConfig cfg){
-        motor = cfg.getSlides();
+        servo = cfg.getRotator();
+        motor = cfg.getMotor();
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-
     public void up1Pixel(){
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor.setTargetPosition(motor.getCurrentPosition() + pixelHeight);
@@ -24,7 +29,6 @@ public class Slides {
         motor.setPower(slideSpeed);
         lastPosition++;
     }
-
     public void down1Pixel(){
         if(lastPosition == 0) return;
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -33,32 +37,52 @@ public class Slides {
         motor.setPower(slideSpeed);
         lastPosition--;
     }
-
     public void goToLastPosition(){
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor.setTargetPosition(pixelHeight * lastPosition);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setPower(slideSpeed);
     }
-
     public void reset(){
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor.setTargetPosition(0);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setPower(slideSpeed);
     }
-
     public void resetLastPosition(){
         lastPosition = 0;
     }
-
     public void setPower(float power){
+        if(power == 0 && motor.getMode() == DcMotor.RunMode.RUN_TO_POSITION){
+            return;
+        }
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor.setPower(power * slideSpeed);
     }
-
+    public void updateClawServo(){
+        if(motor.getCurrentPosition() < slidePosToMoveServo){
+            servo.setPosition(servoScorePos);
+        }else if(motor.getCurrentPosition() < -200) {
+            servo.setPosition(0.49);
+        }else{
+            servo.setPosition(servoStartPos);
+        }
+    }
+    public void resetRotator(){
+        servo.setPosition(servoStartPos);
+    }
+    public void scoreRotator(){
+        servo.setPosition(servoScorePos);
+    }
+    public void gotoPosition(){
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setTargetPosition(-1700);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(slideSpeed);
+    }
     public void telem(Telemetry t){
         t.addLine("Slides");
+        t.addData("power", motor.getPower());
         t.addData("last position", lastPosition);
         t.addData("current position", motor.getCurrentPosition());
         t.addData("target position", motor.getTargetPosition());
